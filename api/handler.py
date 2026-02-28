f71405139a0b16bf3ccc3f14f595b1b27cfab7cc
Terry Ott <Terry.Ott@sixbuckssolutions.com>
2026-02-28 20:12:09 +0000

    Getting mgmt API client didn't puke

--- api/handler.py
+++ api/handler.py
@@ -2,8 +2,7 @@ import json
 import logging
 import os
 
-import kinde_sdk.auth.async_oauth
-
+import kinde_sdk.management
 
 logger = logging.getLogger()
 logger.setLevel("INFO")
@@ -28,9 +27,40 @@ def ping(event, context):
 
 
 def user_get(event, context):
+    if 'requestContext' not in event or                                             \
+            'authorizer' not in event['requestContext'] or                          \
+            'jwt' not in event['requestContext']['authorizer'] or                   \
+            'claims' not in event['requestContext']['authorizer']['jwt'] or         \
+            'sub' not in event['requestContext']['authorizer']['jwt']['claims']:
+        body = {
+            "error": "event['requestContext']['authorizer']['jwt']['claims']['sub'] not passed to Lambda",
+        }
+
+        return {
+            "statusCode"    : 500,
+            "headers": {
+                "Access-Control-Allow-Origin": "https://jamstack-auth.publicntp.net",
+                "Access-Control-Allow-Credentials": True,
+                "Content-Type": "application/json",
+            },
+            "body"          : json.dumps(body, indent=4, sort_keys=True) + "\n",
+        }
+
+    client_oauth_claim_sub: str = event['requestContext']['authorizer']['jwt']['claims']['sub']
+
+    logger.info(f"Received authorized API call from sub = \"{client_oauth_claim_sub}\"")
+
+
+    # Get info about our user, taken from sub field of JWT claims that APIGW put in the event context
+    
+    # Get a client to the management API
+    mgmt_client = kinde_sdk.management.ManagementClient(
+        domain="sixbuckssolutionsllc.kinde.com",
+        client_id=os.environ.get("MGMT_API_CLIENT_ID"),
+        client_secret=os.environ.get("MGMT_API_CLIENT_SECRET"),
+    )
+ 
     env_var_names: list[str] = [
-        "KINDE_CLIENT_ID",
-        "KINDE_CLIENT_SECRET",
         "MGMT_API_CLIENT_ID",
         "MGMT_API_CLIENT_SECRET",
     ]
@@ -39,9 +69,6 @@ def user_get(event, context):
     for curr_name in env_var_names:
         env_vars[curr_name] = os.environ.get(curr_name)
 
-    # Initialize Kinde AsyncOAuth client
-    oauth_client = kinde_sdk.auth.async_oauth.AsyncOAuth()
- 
     body = {
         "env_vars": env_vars,
     }
